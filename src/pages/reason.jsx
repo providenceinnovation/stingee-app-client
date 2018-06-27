a91fe626e9bec27f422e7094de8da2f5228e1f66
Jordan Kelly <jordan.kelly@providence.org>
2018-06-27 13:56:53 -0700

    Cleanup reason for visit page

--- src/pages/reason.jsx
+++ src/pages/reason.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
@@ -6,33 +6,18 @@ import Grid from '@material-ui/core/Grid'
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Divider from '@material-ui/core/Divider';
-import TextField from '@material-ui/core/TextField';
 import Input from '@material-ui/core/Input';
-import InputLabel from '@material-ui/core/InputLabel';
-import Typography from '@material-ui/core/Typography';
 
-import { fetchSearch } from 'redux/modules/codeSearch.js';
+import { fetchSearch } from 'redux/modules/codeSearch';
 import Header from 'components/Header/Header';
 import styles from './reason.less';
 
 @connect(({ codeSearch }) => ({ codeSearch }))
 export default class Reason extends Component {
-  componentDidMount() {
-    const { dispatch } = this.props;
-  }
-
-  search = (event) => {
-    const { dispatch = {}, codeSearch } = this.props;
-    let searchText = event.target.value;
-    let outputField = document.getElementsByClassName('search__results')[0];
-
-    // // outputField.innerHTML = searchText;
+  search = ({ target: { value: searchText = '' } = {} } = {}) => {
+    const { dispatch = {} } = this.props;
     dispatch(fetchSearch({ searchText, zip: "12345" }));
   };
-
-  renderCard = () => {
-
-  }
 
   render() {
     const { codeSearch: { data: codeSearch = []  } }  = this.props;
@@ -41,35 +26,35 @@ export default class Reason extends Component {
       <div>
         <Header title="Stingee" />
         <div className={styles.content}>
-          <Grid>
+          <Grid container spacing={16} noWrap>
             <Grid item xs={12} align="center">
               <Grid item xs={12} md={10} lg={8} xl={6} align="left">
                 <h3>Reason for visit</h3>
-              </Grid>
-            </Grid>
-            <Grid item xs={12} align="center">
-              <Grid item xs={12} md={10} lg={8} xl={6} align="left">
-                <Input onChange={this.search} />
-              </Grid>
-            </Grid>
-            <Grid item xs={12} align="center">
-              <Grid item xs={12} md={10} lg={8} xl={6} align="left">
-
                 <Card>
-                  <CardContent className="search__results">
-                    { codeSearch.length > 0 && <div>Your estimated cost is </div> }
-                    {
-                      codeSearch.map(item => (
-                        <div>
-                          <b>${item.cost}</b> for <b>{item.description}</b>
-                          <Divider/>
-                        </div>
-                      ))
-                    }
+                  <CardContent>
+                    <Input className={styles.search} placeholder="Enter your search term (e.g. Rabies)" onChange={this.search} fullWidth />
                   </CardContent>
                 </Card>
               </Grid>
             </Grid>
+            { codeSearch.length > 0 && (
+              <Grid item xs={12} align="center">
+                <Grid item xs={12} md={10} lg={8} xl={6} align="left">
+                  <Card>
+                    <CardContent className="search__results">
+                      <div>Your estimated cost is:</div>
+                      {
+                        codeSearch.map(item => (
+                          <div>
+                            <b>${item.cost}</b> for <b>{item.description}</b>
+                          </div>
+                        ))
+                      }
+                    </CardContent>
+                  </Card>
+                </Grid>
+              </Grid>
+            )}
           </Grid>
         </div>
       </div>
