e98ba050f0408c25502e51c0e3bf1e2164b18c2b
Jordan Kelly <jordan.kelly@providence.org>
2018-06-27 14:04:02 -0700

    Add hardcoded list until server is working

--- src/redux/modules/payments.js
+++ src/redux/modules/payments.js
@@ -26,14 +26,14 @@ export function fetchPayments () {
     const url = `https://stingee-api-app.herokuapp.com/users/${USER_ID}/payments`;
 
     try {
-      const { data } = await fetchAPI(url) || {};
-      // const data = [
-      //   { _id: 1, cost: '47100', provider: 'Dr. Dredd', location: 'Redmond, WA', date: '04/08/2018', status: 'paid' },
-      //   { _id: 2, cost: '8800', provider: 'Dr. Pain', location: 'Redmond, WA', date: '02/11/2018', status: 'paid' },
-      //   { _id: 3, cost: '9800', provider: 'Dr. Pain', location: 'Seattle, WA', date: '12/01/2017', status: 'paid' },
-      //   { _id: 4, cost: '4500', provider: 'Dr. Pain', location: 'Redmond Sprained Neck Doctor', date: '03/27/2018', status: 'unpaid' },
-      //   { _id: 5, cost: '9600', provider: 'Dr. Pain', location: 'Redmond Sprained Neck Doctor', date: '02/25/2018', status: 'unpaid' },
-      // ];
+      // const { data } = await fetchAPI(url) || {};
+      const data = [
+        { _id: 1, cost: '47100', provider: 'Dr. Dredd', location: 'Redmond, WA', date: '04/08/2018', status: 'paid' },
+        { _id: 2, cost: '8800', provider: 'Dr. Pain', location: 'Redmond, WA', date: '02/11/2018', status: 'paid' },
+        { _id: 3, cost: '9800', provider: 'Dr. Pain', location: 'Seattle, WA', date: '12/01/2017', status: 'paid' },
+        { _id: 4, cost: '4500', provider: 'Dr. Pain', location: 'Redmond Sprained Neck Doctor', date: '03/27/2018', status: 'unpaid' },
+        { _id: 5, cost: '9600', provider: 'Dr. Pain', location: 'Redmond Sprained Neck Doctor', date: '02/25/2018', status: 'unpaid' },
+      ];
 
       dispatch(receivePayments(data));
 
